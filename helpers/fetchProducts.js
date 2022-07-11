e2c052b39f7fe1a3f5863b8c0a201d38233d1826
Helton Pereira <helton.bfsm@gmail.com>
2022-07-11 16:53:22 -0300

    Requisito 1 cumprido

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,11 @@
-const fetchProducts = () => {
-  // seu código aqu
+const fetchProducts = async (productName) => {
+  if (productName === undefined) {
+    throw new Error('You must provide an URL');
+  }
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${productName}`;
+    const result = await fetch(url);
+    const data = await result.json();
+    return data;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,32 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqu
-  fail('Teste vazio');
+  it('should be a funtcion', () => {
+    expect.assertions(1);
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('should "fetch" when called with "computador" as an argument', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('should go to the correct endpoint', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador')
+    const response = `https://api.mercadolibre.com/sites/MLB/search?q=computador`;
+    expect(fetch).toBeCalledWith(response);
+  });
+  it('should have a return similar to "computadorSearch"', async () => {
+    expect.assertions(1);
+    const x = await fetchProducts('computador')
+    expect(x).toEqual(computadorSearch);
+  });
+  it('should return an error message if not argument is provided', async () => {
+    // expect.assertions(1);
+    try {
+      await fetchProducts()
+    } catch (error) {
+      expect(error).toEqual(new Error('You must provide an URL'))
+    }
+  });
 });
