1b753f2f8f49650e93848f7bcd68173826447bee
Helton Pereira <helton.bfsm@gmail.com>
2022-07-13 16:35:09 -0300

    Requisito 6 cumprido

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,5 +4,12 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  
+  it('should call localStorage.setItem', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalled();
+  });
+  it('should...', () => {
+    saveCartItems('<ol><li>Item</li></ol>')
+    expect(localStorage.setItem).toBeCalledWith('cartItems', '<ol><li>Item</li></ol>')
+  });
 });
